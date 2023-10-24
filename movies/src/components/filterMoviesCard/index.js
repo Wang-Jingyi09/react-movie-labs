92d7fb0a4ebbeb9f8d62322636eb12a22871a29f
Jingyi Wang <20104728@email.wit.ie>
2023-10-24 19:36:17 +0100

    Using react-query to manage the browser-based cache.

--- movies/src/components/filterMoviesCard/index.js
+++ movies/src/components/filterMoviesCard/index.js
@@ -9,8 +9,10 @@ import SearchIcon from "@mui/icons-material/Search";
 import FormControl from "@mui/material/FormControl";
 import Select from "@mui/material/Select";
 import img from '../../images/pexels-dziana-hasanbekava-5480827.jpg'
-import React, { useState, useEffect } from "react";
+import React from "react";
 import { getGenres } from "../../api/tmdb-api";
+import { useQuery } from "react-query";
+import Spinner from '../spinner'
 
 const formControl =
 {
@@ -26,25 +28,51 @@ export default function FilterMoviesCard(props) {
   //   {id: 2, name: "Comedy"},
   //   {id: 3, name: "Thriller"}
   // ]
-  const [genres, setGenres] = useState([{ id: '0', name: "All" }])
+  // const [genres, setGenres] = useState([{ id: '0', name: "All" }])
 
-  useEffect(() => {
-    getGenres().then((allGenres) => {
-      setGenres([genres[0], ...allGenres]);
-    });
-    // eslint-disable-next-line react-hooks/exhaustive-deps
-  }, [])
+  // useEffect(() => {
+  //   getGenres().then((allGenres) => {
+  //     setGenres([genres[0], ...allGenres]);
+  //   });
+  //   // eslint-disable-next-line react-hooks/exhaustive-deps
+  // }, [])
+
+  // const handleChange = (e, type, value) => {
+  //   e.preventDefault()
+  //   // Completed later
+  //   props.onUserInput(type, value)   // NEW
+  // };
+  // const handleTextChange = e => {
+  //   handleChange(e, "name", e.target.value)
+  // }
+  // const handleGenreChange = e => {
+  //   handleChange(e, "genre", e.target.value)
+  // };
+  const { data, error, isLoading, isError } = useQuery("genres", getGenres);
+
+  if (isLoading) {
+    return <Spinner />;
+  }
+
+  if (isError) {
+    return <h1>{error.message}</h1>;
+  }
+  const genres = data.genres;
+  if (genres[0].name !== "All"){
+    genres.unshift({ id: "0", name: "All" });
+  }
 
   const handleChange = (e, type, value) => {
-    e.preventDefault()
-    // Completed later
-    props.onUserInput(type, value)   // NEW
+    e.preventDefault();
+    props.onUserInput(type, value); // NEW
   };
-  const handleTextChange = e => {
-    handleChange(e, "name", e.target.value)
-  }
-  const handleGenreChange = e => {
-    handleChange(e, "genre", e.target.value)
+
+  const handleTextChange = (e, props) => {
+    handleChange(e, "name", e.target.value);
+  };
+
+  const handleGenreChange = (e) => {
+    handleChange(e, "genre", e.target.value);
   };
 
   return (
